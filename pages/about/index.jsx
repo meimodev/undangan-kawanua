d62067bf6a1f9aa9620ebfebdace0a568f968f9e
meimo <meimodev@gmail.com>
2021-11-27 09:17:12 +0800

    init

---
+++ pages/about/index.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div className="text-xl text-blue-700">
+      This is About PAge
+      <p>something</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/from/index.jsx
@@ -0,0 +1,5 @@
+const From = () => {
+  return <div className="text-xl text-green-700">This is The FROM PAGE</div>;
+};
+
+export default From;
