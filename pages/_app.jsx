86fab61e8343cdc771f9eba77a743ad00d6ee25b
meimodev <meimodev@gmail.com>
2023-03-14 08:59:40 +0800

    added vercel analytics package

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,13 @@
 import "../styles/global.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+	return (
+		<>
+			<Component {...pageProps} />
+			<Analytics />
+		</>
+	);
 }
 
 export default MyApp;
